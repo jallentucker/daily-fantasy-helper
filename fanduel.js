2d9422c89b2155028777a866fb74c6983506b676
J. Allen Tucker <j.allentucker@gmail.com>
2014-06-13 16:15:00 -0700

    abstracted a bit

--- fanduel.js
+++ fanduel.js
@@ -3,17 +3,14 @@
 
   var loadData = function() {
     var uploads = 0;
-    var batterElement = document.getElementById('batter-file');
-    var pitcherElement = document.getElementById('pitcher-file');
-    var fanduelElement = document.getElementById('fanduel-file');
     var incrementUploads = function() {
       uploads++;
-      $(document.getElementById('uploads')).text('Uploads: ' + uploads);
-      if (uploads === 1) { console.log(document.getElementById('batter-file').value); }
+      $('#uploads').text('Uploads: ' + uploads);
+      if (uploads === 1) { console.log($('#batter-file')); }
     };
-    $(batterElement).change(function() { incrementUploads(); });
-    $(pitcherElement).change(function() { incrementUploads(); });
-    $(fanduelElement).change(function() { incrementUploads(); });
+    $('#batter-file').change(function() { incrementUploads(); });
+    $('#pitcher-file').change(function() { incrementUploads(); });
+    $('#fanduel-file').change(function() { incrementUploads(); });
   };
 
   $(function() {
