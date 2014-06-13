daf690c3a39608b9590157fdf467f0d52de338ae
J. Allen Tucker <j.allentucker@gmail.com>
2014-06-13 16:00:01 -0700

    started working on loadData function

--- fanduel.js
+++ fanduel.js
@@ -1,13 +1,23 @@
 (function() {
   'use strict';
 
-  var printCurrentDateAsTitle = function(today) {
-    $('title').append(today.getMonth().toString() + today.getDate().toString());
+  var loadData = function() {
+    var uploads = 0;
+    var batterElement = document.getElementById('batter-file');
+    var pitcherElement = document.getElementById('pitcher-file');
+    var fanduelElement = document.getElementById('fanduel-file');
+    var incrementUploads = function() {
+      uploads++;
+      $(document.getElementById('uploads')).text('Uploads: ' + uploads);
+      if (uploads === 1) { console.log(document.getElementById('batter-file').value); }
+    };
+    $(batterElement).change(function() { incrementUploads(); });
+    $(pitcherElement).change(function() { incrementUploads(); });
+    $(fanduelElement).change(function() { incrementUploads(); });
   };
 
   $(function() {
-    var today = new Date();
-    printCurrentDateAsTitle(today);
+    loadData();
   });
 
 })();
